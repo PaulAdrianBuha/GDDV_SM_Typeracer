ab0337b4479bf54e0c22b353737b00b07654115a
RayoROAR <paul.adrian.buha@gmail.com>
2024-10-10 23:15:27 +0200

    recovery done, and other

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 
-define("EMAIL_FROM", "gddvsmtp@gmail.com");
+var_dump(setlocale(LC_ALL, 'ca_ES'));
+define("EMAIL_FROM", "gddvsmtp@gmail.com"); // constant
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
@@ -23,8 +24,8 @@ $configuration = array(
     '{LOGIN_LOGOUT_TEXT}' => 'Identificar-me',
     '{LOGIN_LOGOUT_URL}'  => '/?page=login',
     '{REGISTER_URL}'      => '/?page=register',
+    '{RECOVERY_URL}'      => '/?page=recovery',
     '{SITE_NAME}'         => 'La meva pàgina',
-    '{HOME_SECOND_BUTTON_DISPLAY}' => 'none'
 );
 
 // parameter processing
@@ -38,21 +39,29 @@ if ($method == 'POST') {
 }
  
 if (isset($parameters['page'])) {
-    // When user navigates to specific diferent page 
-    getPage($template, $configuration, $parameters);
+    // When user navigates to a diferent specific form page (register, login, recovery) 
+    getPage($template, $db_connection, $configuration, $parameters);
 } else if (isset($parameters['register'])) {
-    // When user submits register form
+    // When user submits the register form
     postRegister($template, $db_connection, $configuration, $parameters);
 } else if (isset($parameters['login'])) {
-    // When user submits login form
+    // When user submits the login form
     postLogin($template, $db_connection, $configuration, $parameters);
 } else if (isset($parameters['verifyEmail'])) {
+    // When user presses the verify button from the email
     verifyAccount($template, $db_connection, $configuration, $parameters);
 } else if (isset($parameters['resendVerifyEmail'])) {
-   resendVerifyEmail($template, $db_connection, $configuration, $parameters);
+    // When user tries to login without verifying or when they press the resend verification email button
+    resendVerifyEmail($template, $db_connection, $configuration, $parameters);
+} else if (isset($parameters['recovery'])) {
+    // When user submits the account recovery form
+    postRecovery($template, $db_connection, $configuration, $parameters);
+} else if (isset($parameters['changePasswordConfirm'])) {
+    // When user confirms the password change
+    changePassword($template, $db_connection, $configuration, $parameters);
 } else {
-     // default page view when first entering
-     printHtml($template, $configuration);
+    // default page view when first entering
+    printHtml($template, $configuration);
 }
 
 // FUNCTIONS -----------------------------------------------------------------------------------------------------------------
@@ -62,58 +71,91 @@ function printHtml($template, $configuration) {
     echo getHtml($template, $configuration);
 }
 
+// retrieve the html (with replacements) given a template
 function getHtml($template, $configuration) {
     $html = file_get_contents('plantilla_' . $template . '.html', true);
     $html = str_replace(array_keys($configuration), array_values($configuration), $html);
     return $html;
 }
 
-function getPage($template, $configuration, $parameters) {
+// (VIEW) Either the register, the login page, or the password recovery page. Before submitting any form
+function getPage($template, $db_connection, $configuration, $parameters) {
     if ($parameters['page'] == 'register') {
         $template = 'register';
         $configuration['{REGISTER_USERNAME}'] = '';
         $configuration['{LOGIN_LOGOUT_TEXT}'] = 'Ja tinc un compte';
+
     } else if ($parameters['page'] == 'login') {
         $template = 'login';
         $configuration['{LOGIN_USERNAME}'] = '';
+
+    } else if ($parameters['page'] == 'recovery') {
+        $template = 'recovery';
+        $configuration['{FEEDBACK}'] = "Introdueix el teu nom d'usuari o correu electrònic i rebràs un email per canviar la contraseña.";
+        
+    } else if ($parameters['page'] == 'change_password') {
+        // Executed when pressing the change password button from the email
+        $db = new PDO($db_connection);
+        $sql = 'SELECT * FROM users WHERE (user_verification_code = :user_verification_code AND user_email = :user_email)';
+        $query = $db->prepare($sql);
+        $query->bindValue(':user_verification_code', $parameters['user_verification_code']);
+        $query->bindValue(':user_email', $parameters['user_email']);
+        $query->execute();
+        $result_row = $query->fetch();
+        if ($result_row) {
+            $template = 'change_password';
+            $configuration['{USER_VERIFICATION_CODE}'] = $parameters['user_verification_code'];
+            $configuration['{USER_EMAIL}'] = $parameters['user_email'];
+        }
+        else {
+            //$template = 'home';
+            $configuration['{FEEDBACK}'] = "S'ha produït un error en el procés de recuperació.";
+        }
     }
+
     printHtml($template, $configuration);
 }
 
+// (VIEW) Executed after submitting the registration form
+// Uses plantilla_reverify
 function postRegister($template, $db_connection, $configuration, $parameters) {
     if (strlen($parameters['user_password']) < 8) {
         $configuration['{FEEDBACK}'] = "<mark>ERROR: No s'ha pogut crear el compte <b>"
                 . htmlentities($parameters['user_name']) . '</b> La contrasenya ha de ser de 8 caràcters com a mínim</mark>';
+    } else if ($parameters['user_password'] != $parameters['user_repeat_password']) {
+        $configuration['{FEEDBACK}'] = "<mark>ERROR: Les contrasenyes no coincideixen</mark>";
     } else {
-        // Generamos un codigo random para verificar que enviaremos al correo y asociamos al user en la base de datos
-        $verificationcode = strval(random_int(0, 9999999));
+        $template = 'reverify';
+         // Generamos un codigo random para verificar que enviaremos al correo y asociamos al user en la base de datos
+         $verificationcode = strval(random_int(0, 9999999));
 
-        $db = new PDO($db_connection);
-        $sql = 'INSERT INTO users (user_name, user_password, user_email, user_verification_code) VALUES (:user_name, :user_password, :user_email, :user_verification_code)';
-        $query = $db->prepare($sql);
-        $query->bindValue(':user_name', $parameters['user_name']);
-        $query->bindValue(':user_password', password_hash($parameters['user_password'], PASSWORD_BCRYPT));
-        $query->bindValue(':user_email', $parameters['user_email']);
-        $query->bindValue(':user_verification_code', $verificationcode);
-        try {
-            $query->execute();
-            // TODO NO DEJAR INICIAR HASTA QUE VERIFIQUE LA CUENTA, MOSTRAR ALGUNA TEMPLATE DE plantilla_feedback.html MAYBE?
-            $configuration['{FEEDBACK}'] = 'Verifica el teu compte:  <b>' . htmlentities($parameters['user_name']) . ' <br/> ' . htmlentities($parameters['user_email']) . '</b>';
-            $configuration['{LOGIN_LOGOUT_TEXT}'] = "Torna a l'inici";
-            $configuration['{LOGIN_LOGOUT_URL}'] = '/';
-
-            sendVerificationEmail($parameters['user_email'], $verificationcode);
-        } catch (PDOException $e) {
-             // Això no s'executarà mai (???)
-             $configuration['{FEEDBACK}'] = "<mark>ERROR: No s'ha pugut crear el compte <b>"
-             . htmlentities($parameters['user_name']) . '</b></mark>';
-        }
+         $db = new PDO($db_connection);
+         $sql = 'INSERT INTO users (user_name, user_password, user_email, user_verification_code) VALUES (:user_name, :user_password, :user_email, :user_verification_code)';
+         $query = $db->prepare($sql);
+         $query->bindValue(':user_name', $parameters['user_name']);
+         $query->bindValue(':user_password', password_hash($parameters['user_password'], PASSWORD_BCRYPT));
+         $query->bindValue(':user_email', $parameters['user_email']);
+         $query->bindValue(':user_verification_code', $verificationcode);
+         try {
+             $query->execute();
+             $configuration['{FEEDBACK}'] = 'Revisa el correu per verificar el teu compte:  <b>' . htmlentities($parameters['user_name']) . ' <br/> ' . htmlentities($parameters['user_email']) . '</b>';
+             $configuration['{LOGIN_LOGOUT_TEXT}'] = "Torna a l'inici";
+             $configuration['{LOGIN_LOGOUT_URL}'] = '/';
+             $configuration['{HOME_SECOND_BUTTON_URL}'] = "/?resendVerifyEmail=true&user_verification_email=" . urlencode($parameters['user_email']);
+ 
+             sendVerificationEmail($parameters['user_email'], $verificationcode);
+         } catch (PDOException $e) {
+              // Això no s'executarà mai (???)
+              $configuration['{FEEDBACK}'] = "<mark>ERROR: No s'ha pugut crear el compte <b>"
+              . htmlentities($parameters['user_name']) . '</b></mark>';
+         }
     }
     printHtml($template, $configuration);
 }
 
+// (VIEW) Executed after submitting the login form
+// Uses plantilla_home if the user is verified and plantilla_reverify if not
 function postLogin($template, $db_connection, $configuration, $parameters) {
-    // When user submits login form
     $db = new PDO($db_connection);
     // TODO CHECK USER IS VERIFIED
     $sql = 'SELECT * FROM users WHERE (user_name = :user_name OR user_email = :user_name)';
@@ -124,23 +166,46 @@ function postLogin($template, $db_connection, $configuration, $parameters) {
     $result_row = $query->fetch();
     if ($result_row && password_verify($parameters['user_password'], $result_row['user_password'])) {
         if ($result_row['user_verified'] == 0) {
-            $configuration['{FEEDBACK}'] = "Revisa el correu per verificar el compte";
+            $template = 'reverify';
+            $configuration['{FEEDBACK}'] = 'Revisa el correu per verificar el teu compte:  <b>' . htmlentities($result_row['user_name']) . ' <br/> ' . htmlentities($result_row['user_email']) . '</b>';
             $configuration['{LOGIN_LOGOUT_TEXT}'] = "Torna a l'inici";
             $configuration['{LOGIN_LOGOUT_URL}'] = '/';
             $configuration['{HOME_SECOND_BUTTON_URL}'] = "/?resendVerifyEmail=true&user_verification_email=" . urlencode($result_row['user_email']);
-            $configuration['{HOME_SECOND_BUTTON_DISPLAY}'] = "block";
-            $configuration['{HOME_SECOND_BUTTON_TEXT}'] = "Reenviar correu verificació";
         } else {
+            //$template = 'home';
             $configuration['{FEEDBACK}'] = '"Sessió" iniciada com <b>' . htmlentities($result_row['user_name']) . ' <br/> ' . htmlentities($result_row['user_email']) . '</b>';
             $configuration['{LOGIN_LOGOUT_TEXT}'] = 'Tancar "sessió"';
             $configuration['{LOGIN_LOGOUT_URL}'] = '/?page=logout';
         }
     } else {
+        //$template = 'home';
         $configuration['{FEEDBACK}'] = '<mark>ERROR: Usuari desconegut o contrasenya incorrecta</mark>';
     }
     printHtml($template, $configuration);
 }
 
+// (VIEW) Executed after submitting the account recovery form
+// Uses plantilla_recovery (loops back to it)
+function postRecovery($template, $db_connection, $configuration, $parameters) {
+    $template = 'recovery';
+    $db = new PDO($db_connection);
+    $sql = 'SELECT * FROM users WHERE (user_name = :recovery_name OR user_email = :recovery_name)';
+    $query = $db->prepare($sql);
+    $query->bindValue(':recovery_name', $parameters['recovery_name']);
+    $query->execute();
+    $result_row = $query->fetch();
+    if ($result_row) {
+        $configuration['{FEEDBACK}'] = 'Revisa el correu per canviar la contrasenya:  <b>' . htmlentities($result_row['user_name']) . ' <br/> ' . htmlentities($result_row['user_email']) . '</b>';
+    
+        sendRecoveryEmail($result_row['user_email'], $result_row['user_verification_code']);
+    } else {
+        $configuration['{FEEDBACK}'] = "<mark>ERROR: No s'ha pogut enviar el correu de recuperació.</mark>";
+    }
+    printHtml($template, $configuration);
+}
+
+// (VIEW) Executed when pressing the verify button from the email
+// Uses plantilla_home, because it acts as logging into the account
 function verifyAccount($template, $db_connection, $configuration, $parameters) {
     $db = new PDO($db_connection);
     $sql = 'SELECT * FROM users WHERE (user_verification_code = :user_verification_code AND user_email = :user_verification_email)';
@@ -165,15 +230,21 @@ function verifyAccount($template, $db_connection, $configuration, $parameters) {
     printHtml($template, $configuration);
 }
 
+// Sends a specific verification email to the specified email. Automatically executed after registering
 function sendVerificationEmail($emailTo, $verificationcode) {
     $verificationTemplateVars = [
         "{VERIFICATION_CODE}" => $verificationcode,
-        "{VERIFICATION_EMAIL}" => urlencode($emailTo) // encodifiquem el paràmetre per a que correus com exemple+1@gmail.com funcionin
+        "{VERIFICATION_EMAIL}" => urlencode($emailTo), // encodifiquem el paràmetre per a que correus com exemple+1@gmail.com funcionin
+        "{TIMESTAMP}" => date('l jS \of F Y h:i:s A') // afegim una variable que sigui diferent per cada correu per a que no s'amagui a partir de la 2a vegada
     ];
     sendEmail($emailTo, "Verifica el teu compte", getHtml('verification_email', $verificationTemplateVars));
 }
 
+// (VIEW) Executed when trying to log in without having verified the account beforehand or when choosing to resend the verification email.
+// Is basically a page acting as barrier, the "resend verification email" option is displayed
+// Uses plantilla_reverify
 function resendVerifyEmail($template, $db_connection, $configuration, $parameters) {
+    $template = 'reverify';
     $db = new PDO($db_connection);
     $sql = 'SELECT * FROM users WHERE (user_email = :user_verification_email)';
     $query = $db->prepare($sql);
@@ -181,12 +252,10 @@ function resendVerifyEmail($template, $db_connection, $configuration, $parameter
     $query->execute();
     $result_row = $query->fetch();
     if ($result_row) {
-        $configuration['{FEEDBACK}'] = "Revisa el correu per verificar el compte";
+        $configuration['{FEEDBACK}'] = 'Revisa el correu per verificar el teu compte:  <b>' . htmlentities($result_row['user_name']) . ' <br/> ' . htmlentities($result_row['user_email']) . '</b>';
         $configuration['{LOGIN_LOGOUT_TEXT}'] = "Torna a l'inici";
         $configuration['{LOGIN_LOGOUT_URL}'] = '/';
         $configuration['{HOME_SECOND_BUTTON_URL}'] = "/?resendVerifyEmail=true&user_verification_email=" . urlencode($result_row['user_email']);
-        $configuration['{HOME_SECOND_BUTTON_DISPLAY}'] = "block";
-        $configuration['{HOME_SECOND_BUTTON_TEXT}'] = "Reenviar correu verificació";
     
         sendVerificationEmail($result_row['user_email'], $result_row['user_verification_code']);
     } else {
@@ -195,10 +264,17 @@ function resendVerifyEmail($template, $db_connection, $configuration, $parameter
     printHtml($template, $configuration);
 }
 
-function sendRecoveryEmail($emailTo) {
-    sendEmail($emailTo, "Recupera el teu compte", getHtml('recovery_email', []));
+// Sends a specific email for recovering your password
+function sendRecoveryEmail($emailTo, $verificationcode) {
+    $recoveryTemplateVars = [
+        "{VERIFICATION_CODE}" => $verificationcode,
+        "{EMAIL}" => urlencode($emailTo), // encodifiquem el paràmetre per a que correus com exemple+1@gmail.com funcionin
+        "{TIMESTAMP}" => date('l jS \of F Y h:i:s A') // afegim una variable que sigui diferent per cada correu per a que no s'amagui a partir de la 2a vegada
+    ];
+    sendEmail($emailTo, "Recupera el teu compte", getHtml('recovery_email', $recoveryTemplateVars));
 }
 
+// Generic funtion for sending emails
 function sendEmail($emailTo, $subject, $message) {
     $mail = new PHPMailer(true);
 
@@ -223,8 +299,29 @@ function sendEmail($emailTo, $subject, $message) {
         $mail->Body  = $message;
         $mail->AltBody = $message;
         $mail->send();
-        echo 'El missatge s\'ha enviat';
     } catch (Exception $e) {
-        echo "El missatge no s'ha enviat. Mailer Error: {$mail->ErrorInfo}";
+        echo "El missatge no s'ha pogut enviar. Mailer Error: {$mail->ErrorInfo}";
     }
+}
+
+function changePassword($template, $db_connection, $configuration, $parameters) {
+    if (strlen($parameters['user_password']) < 8) {
+        $configuration['{FEEDBACK}'] = "<mark>ERROR: No s'ha pogut canviar la contrasenya. La contrasenya ha de ser de 8 caràcters com a mínim.</mark>";
+    } else if ( $parameters['user_recover_password'] != $parameters['user_repeat_password']) {
+        $configuration['{FEEDBACK}'] = "<mark>ERROR: Les contrasenyes no coincideixen</mark>";
+    } else {
+        $db = new PDO($db_connection);
+        $sql = 'UPDATE users SET user_password = :user_recover_password WHERE (user_email = :user_email AND user_verification_code = :user_verification_code)';
+        $query = $db->prepare($sql);
+        $query->bindValue(':user_recover_password', password_hash($parameters['user_recover_password'], PASSWORD_BCRYPT));
+        $query->bindValue(':user_email', $parameters['user_email']);
+        $query->bindValue(':user_verification_code', $parameters['user_verification_code']);
+        try {
+            $query->execute();
+            $configuration['{FEEDBACK}'] = "S'ha canviat la contrasenya exitosament";
+        } catch (PDOException $e) {
+            $configuration['{FEEDBACK}'] = "<mark>ERROR: No s'ha pogut canviar la contrasenya</mark>";
+        }
+    }
+    printHtml($template, $configuration);
 }
